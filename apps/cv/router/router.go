33775b5cde5653a70151653c481e5a717b1de100
joshsummers4 <67658350+joshsummers4@users.noreply.github.com>
2025-08-21 14:06:07 +0000

    update to router file so the router is only registered once

--- apps/cv/router/router.go
+++ apps/cv/router/router.go
@@ -8,14 +8,20 @@ import (
 	"github.com/joshsummers4/golang_cv/libs/utils/logger"
 )
 
-var mux *http.ServeMux = http.NewServeMux()
+var (
+	mux        *http.ServeMux
+	loggingMux http.Handler
+)
 
-func Router() http.Handler {
-
+func init() {
+	mux = http.NewServeMux()
 	mux.HandleFunc("GET /", home.GetHandler)
 	mux.HandleFunc("GET /contact", contact.GetHandler)
+	loggingMux = logger.LoggingHandler(
+		[]string{"/public", "/favicon.ico", "/.well-known/appspecific/com.chrome.devtools.json"},
+	)(mux)
+}
 
-	loggingMux := logger.LoggingHandler([]string{"/public", "/favicon.ico", "/.well-known/appspecific/com.chrome.devtools.json"})(mux)
-
+func Router() http.Handler {
 	return loggingMux
 }
