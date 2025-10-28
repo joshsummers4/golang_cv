c2d55c2a168b1deb7ce89959ad40cae58ee7721d
Josh Summers <joshsummers4@yahoo.co.uk>
2025-10-28 16:09:14 +0000

    update content and link to demo apps

--- apps/cv/router/pages/home/skills/skills.go
+++ apps/cv/router/pages/home/skills/skills.go
@@ -5,8 +5,8 @@ import (
 	_ "embed"
 	"html/template"
 
-	"github.com/joshsummers4/golang_cv/libs/utils/tpl"
 	"github.com/joshsummers4/golang_cv/libs/design/atoms"
+	"github.com/joshsummers4/golang_cv/libs/utils/tpl"
 )
 
 //go:embed skills.html
@@ -14,31 +14,31 @@ var skillsHTML string
 var skillsTPL = tpl.Parse("skills section", skillsHTML)
 
 type skillsData struct {
-	Languages []template.HTML
-	Databases []template.HTML
-	Tools     []template.HTML
-	Testing   []template.HTML
+	Frontend []template.HTML
+	Backend  []template.HTML
+	DevOps   []template.HTML
+	Testing  []template.HTML
 }
 
 func SkillsHTML(ctx context.Context) template.HTML {
-	Languages:=    []string{"Go", "TypeScript", "JavaScript", "Angular", "AlpineJS", "HTML5", "CSS3", "SASS"}
-	Databases:=    []string{"SQL", "DuckDB", "Parquet files", "MySQL", "Microsoft Access", "DynamoDB"}
-	Tools:=        []string{"Git", "Sourcetree", "Bitbucket", "AWS", "Storyblok", "Tailwind CSS"}
-	Testing:=      []string{"Unit Testing", "Visual Testing", "Agile/Scrum methodolgies"}
+	Frontend := []string{"Angular", "TypeScript", "JavaScript", "HTML5", "CSS3", "SASS", "React", "Tailwind CSS", "Storyblok"}
+	Backend := []string{"Go", "SQL", "DuckDB", "Parquet files", "MySQL", "DynamoDB", "REST API"}
+	DevOps := []string{"Git", "Sourcetree", "Bitbucket", "AWS", "Docker"}
+	Testing := []string{"Unit Testing", "Visual Testing", "Agile/Scrum methodolgies"}
 
-	data:= skillsData{}
-	for _, lang := range Languages {
-		data.Languages = append(data.Languages, atoms.PebbleHtml(ctx, lang))
+	data := skillsData{}
+	for _, lang := range Frontend {
+		data.Frontend = append(data.Frontend, atoms.PebbleHtml(ctx, lang))
 	}
-	for _, db := range Databases {
-		data.Databases = append(data.Databases, atoms.PebbleHtml(ctx, db))
+	for _, db := range Backend {
+		data.Backend = append(data.Backend, atoms.PebbleHtml(ctx, db))
 	}
-	for _, tool := range Tools {
-		data.Tools = append(data.Tools, atoms.PebbleHtml(ctx, tool))
+	for _, tool := range DevOps {
+		data.DevOps = append(data.DevOps, atoms.PebbleHtml(ctx, tool))
 	}
 	for _, test := range Testing {
 		data.Testing = append(data.Testing, atoms.PebbleHtml(ctx, test))
 	}
-	
+
 	return skillsTPL.HTML(ctx, data)
 }
