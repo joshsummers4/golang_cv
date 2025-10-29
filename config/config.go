9eb6cef9fd2ddc55fee04d6699f93abff2a18304
Josh Summers <joshsummers4@yahoo.co.uk>
2025-10-29 20:30:09 +0000

    remove logging

--- config/config.go
+++ config/config.go
@@ -26,8 +26,6 @@ func GetEnvironmentVariables() {
 	APIPASSWORD = os.Getenv("API_PASSWORD")
 	APIURL = os.Getenv("API_URL")
 
-	log.Println("config set up", APIURL, APIUSERNAME)
-
 	if APIUSERNAME == "" || APIPASSWORD == "" || APIURL == "" {
 		log.Fatal("Missing API credentials")
 	}
